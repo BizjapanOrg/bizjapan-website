3476650ed28739771bb667257191f504692ba6d7
hirkame <vjtsx.45afv@gmail.com>
2019-10-30 16:54:18 +0900

    Created AboutUs component and adjusted the style.

---
+++ src/components/AboutUs.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+// Import styled components
+import Container from "../components/styled/Container"
+import Title from "../components/styled/Title"
+
+const AboutUs = () => (
+  <Container>
+    <Title>About Us</Title>
+    <p>
+      Bizjapan is a platform where a wide variety of people gather with a global
+      perspective and entrepreneurship, and a wide variety of projects are
+      running. Bizjapan was founded in 2011 and became an NPO in 2017. We have
+      successfully completed 55 projects so far.
+      <br />
+      <br />
+      Currently, 30 members from about 10 countries, mainly students from the
+      University of Tokyo, Waseda University, and Keio University, and
+      especially this year, under the slogan “Because we love it.” Ten projects
+      are in progress, from supporting studying abroad, local revitalization,
+      data science, art, to African startups and space medicine.
+      <br />
+      <br />
+      Bizjapan is an NPO where students beyond the difference of gender,
+      nationality, university, and interest come together. Here, you can pursue
+      a project of your own, with the belief in yourself and your critical eye.
+    </p>
+  </Container>
+)
+
+export default AboutUs
