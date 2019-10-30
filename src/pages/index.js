f881980ac64cd200df25a91911fda49c0f3ae9ad
hirkame <vjtsx.45afv@gmail.com>
2019-10-30 16:56:05 +0900

    Adjusted the style of AboutUs component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 import SEO from "../components/seo"
 
-// Import styled components
-import Container from "../components/styled/Container"
-import Button from "../components/styled/Button"
-import Title from "../components/styled/Title"
+// import components
+import AboutUs from "../components/AboutUs"
 
 const IndexPage = () => (
-  <Container>
+  <>
     <SEO title="Home" />
-    <Title>Test Title</Title>
-    <p>First Paragraph. Welcome to your new Gatsby site.</p>
-    <p>Second Paragraph. Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Button>Test</Button>
-  </Container>
+    <AboutUs />
+  </>
 )
 
 export default IndexPage
