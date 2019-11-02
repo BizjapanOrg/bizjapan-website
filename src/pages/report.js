6cd6bf6c189cdc6816953f77ec28ff6d75b1c145
hirkame <vjtsx.45afv@gmail.com>
2019-11-02 23:22:14 +0900

    Created all the pages inside the website.

--- src/pages/report.js
+++ src/pages/report.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   // <Layout>
   <>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <SEO title="Report" />
+    <h1>Hi from the Report page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </>
