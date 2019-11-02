155ce50411db022d369f4732a3d485ffc93c7d89
hirkame <vjtsx.45afv@gmail.com>
2019-11-02 23:26:24 +0900

    Added footer navi in Footer component.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,41 @@
-// import { Link } from "gatsby"
 import React from "react"
+import { Link } from "gatsby"
 
-const Footer = () => <footer>This is Footer</footer>
+const Footer = () => (
+  <footer>
+    <p>©2019 NPO Bizjapan</p>
+
+    {/* Footer navi */}
+    <ul>
+      <li>
+        <Link to="/">- HOME</Link>
+      </li>
+      <li>
+        <Link to="/projects">- PROJECT</Link>
+      </li>
+      {/* <li>
+        <a class="fadelink text" href="./profile/">
+          -PROFILE
+        </a>
+      </li>
+      <li>
+        <a class="fadelink text" href="./report/">
+          -REPORT
+        </a>
+      </li>
+      <li>
+        <a
+          class="fadelink text"
+          href="https://sites.google.com/bizjapan.org/recruiting-jp/home"
+        >
+          -JOIN
+        </a>
+      </li>
+      <li>
+        <a href="./">-CONTACT</a>
+      </li> */}
+    </ul>
+  </footer>
+)
 
 export default Footer
