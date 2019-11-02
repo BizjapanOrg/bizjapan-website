4a26e59118ea847ad8c48c8069b501fd6873603d
hirkame <vjtsx.45afv@gmail.com>
2019-11-02 23:30:59 +0900

    Modified a little bit.

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,38 +4,33 @@ import { Link } from "gatsby"
 const Footer = () => (
   <footer>
     <p>©2019 NPO Bizjapan</p>
-
-    {/* Footer navi */}
-    <ul>
-      <li>
-        <Link to="/">- HOME</Link>
-      </li>
-      <li>
-        <Link to="/projects">- PROJECT</Link>
-      </li>
-      {/* <li>
-        <a class="fadelink text" href="./profile/">
-          -PROFILE
-        </a>
-      </li>
-      <li>
-        <a class="fadelink text" href="./report/">
-          -REPORT
-        </a>
-      </li>
-      <li>
-        <a
-          class="fadelink text"
-          href="https://sites.google.com/bizjapan.org/recruiting-jp/home"
-        >
-          -JOIN
-        </a>
-      </li>
-      <li>
-        <a href="./">-CONTACT</a>
-      </li> */}
-    </ul>
+    <FooterNavi />
   </footer>
+)
+
+const FooterNavi = () => (
+  <ul>
+    <li>
+      <Link to="/">- HOME</Link>
+    </li>
+    <li>
+      <Link to="/profile">- PROFILE</Link>
+    </li>
+    <li>
+      <a href="https://sites.google.com/bizjapan.org/recruiting-jp/home">
+        - JOIN
+      </a>
+    </li>
+    <li>
+      <Link to="/project">- PROJECT</Link>
+    </li>
+    <li>
+      <Link to="/report">- REPORT</Link>
+    </li>
+    <li>
+      <Link to="/">- CONTACT</Link>
+    </li>
+  </ul>
 )
 
 export default Footer
