1a482a16f701632cf65de2c75eb012fc0da7e1d3
hirkame <vjtsx.45afv@gmail.com>
2019-11-10 18:52:01 +0900

    Changed the folder for ProjectCard.js and added an image on each project card.

--- src/components/styled/ProjectCard.js
+++ src/components/styled/ProjectCard.js
@@ -1,34 +1,38 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
 const ProjectCard = props => (
   <ProjectCardStyle>
-    <CardName> {props.name} </CardName>
-    <CardDesc> {props.desc} </CardDesc>
+    <img
+      src={props.img}
+      alt={props.name}
+      style={{ borderRadius: `20px 20px 0 0`, width: `100%` }}
+    />
+    <div style={{ margin: `1em` }}>
+      <CardName> {props.name} </CardName>
+      <CardDesc> {props.desc} </CardDesc>
+    </div>
   </ProjectCardStyle>
-)
+);
 
 // Define style of ProjectCard
 const ProjectCardStyle = styled.div`
-  background: blue;
   width: 230px;
   min-width: 230px;
   border-radius: 20px;
-  background-color: white;
   box-shadow: 6px 6px 30px 0 rgba(0, 0, 0, 0.16);
   margin: 0 0.5em 1.45em 0.5em;
-`
+`;
 const CardName = styled.p`
-  padding: 0.5em 1em;
   margin: 0;
   font-size: 1.1em;
   font-weight: bold;
   text-align: center;
-`
+`;
 const CardDesc = styled.p`
-  padding: 0.5em 1em;
   margin: 0;
-  font-size:0.8em;
-`
+  padding-top: 0.5em;
+  font-size: 0.8em;
+`;
 
-export default ProjectCard
+export default ProjectCard;
