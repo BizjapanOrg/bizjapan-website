a54a008549155bfe3dcdc4d8dac670406c7bcea7
hirkame <vjtsx.45afv@gmail.com>
2019-11-10 18:52:30 +0900

    Imported images on Platform.js file and rendered them by ProjectCard.

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -5,7 +5,12 @@ import Container from './styled/Container';
 import Title from './styled/Title';
 
 // Import components
-import ProjectCard from './ProjectCard';
+import ProjectCard from './styled/ProjectCard';
+
+// Import Images
+import jumpjapan from '../images/projects/jumpjapan.png';
+import mxspace from '../images/projects/mxspace.png';
+import nichy from '../images/projects/nichy.png';
 
 const Platform = () => (
   <Container>
@@ -19,14 +24,17 @@ const Platform = () => (
       }}
     >
       <ProjectCard
+        img={jumpjapan}
         name='Jump Japam'
         desc='Providing the real information about studying and working in Japan with students abroad.'
       />
       <ProjectCard
+        img={mxspace}
         name='M x Space'
         desc="Holding space medicine study tours during long holidays in spring and summer In order to realize 'The world guaranteed quality of space life (QOSL)'."
       />
       <ProjectCard
+        img={nichy}
         name='Nichy'
         desc='Promoting exchange in terms of human resources between Japan and China by connecting Japanese university students and Chinese companies.'
       />
