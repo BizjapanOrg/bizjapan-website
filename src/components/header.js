fc6f07c6657511aafb8d13cd7b6808d5b44909ff
hirkame <vjtsx.45afv@gmail.com>
2019-11-10 18:30:06 +0900

    Changed position of Header.

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,6 @@ const Header = () => (
       width: `100%`,
       height: `70px`,
       padding: `20px 5%`,
-      position: `absolute`,
       background: `white`,
       zIndex: `10000`
     }}
