1ce6a605ff2c7494bab34f4394f9f1b005301d36
hirkame <vjtsx.45afv@gmail.com>
2019-11-10 15:52:27 +0900

    Footer as well.

--- src/components/FooterNavi.js
+++ src/components/FooterNavi.js
@@ -1,35 +1,44 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
 // A list of pages
 const FooterNavi = props => {
   const naviItems = props.list.map(item => {
-    if (item === "join") {
+    if (item === 'join') {
       return (
         <li key={item} style={{ width: `33%`, display: `inline-block` }}>
-          <a href="https://sites.google.com/bizjapan.org/recruiting-jp/home">
+          <a
+            style={{ color: `black`, textDecoration: `none` }}
+            href='https://sites.google.com/bizjapan.org/recruiting-jp/home'
+          >
             - JOIN
           </a>
         </li>
-      )
+      );
     }
     return (
       <li key={item} style={{ width: `33%`, display: `inline-block` }}>
-        <Link to={"/" + item}>{"- " + item.toUpperCase()}</Link>
+        <Link
+          style={{ color: `black`, textDecoration: `none` }}
+          to={'/' + item}
+          activeStyle={{ textDecoration: `underline` }}
+        >
+          {'- ' + item.toUpperCase()}
+        </Link>
       </li>
-    )
-  })
+    );
+  });
   return (
     <ul
       style={{
         listStyle: `none`,
         textAlign: `center`,
-        margin: `0`,
+        margin: `0`
       }}
     >
       {naviItems}
     </ul>
-  )
-}
+  );
+};
 
-export default FooterNavi
+export default FooterNavi;
