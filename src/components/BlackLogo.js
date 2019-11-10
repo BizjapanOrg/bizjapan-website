31afb9cad8db022c017699d96d59cea603b02123
hirkame <vjtsx.45afv@gmail.com>
2019-11-10 16:51:02 +0900

    Changed the name of the component from HeaderLogo to BlackLogo.

---
+++ src/components/BlackLogo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const HeaderLogo = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "logo_black.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      alt='Logo Black'
+      imgStyle={{
+        objectFit: 'contain',
+        objectPosition: '50% 50%'
+      }}
+      style={{ height: `30px` }}
+    />
+  );
+};
+
+export default HeaderLogo;
