c5f9bdf909102d71a50040bd559128329edf3cfb
hirkame <vjtsx.45afv@gmail.com>
2019-11-10 16:14:47 +0900

    Removed heilight of Container & Title styled components.

--- src/components/styled/Container.js
+++ src/components/styled/Container.js
@@ -1,16 +1,15 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
 const Container = styled.section`
   position: static;
   box-sizing: border-box;
-  background-color: red;
   width: 65%;
   min-width: 260px;
   padding: 10em 0 0 0;
   margin: 0 auto;
-`
+`;
 
 export default ({ children }) => (
   <Container className={`container`}>{children}</Container>
-)
+);
--- src/components/styled/Title.js
+++ src/components/styled/Title.js
@@ -1,12 +1,11 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
 const Title = styled.h1`
-  background-color: black;
-  color: white;
+  color: black;
   text-align: center;
   letter-spacing: 2px;
   margin-bottom: 50px;
-`
+`;
 
-export default ({ children }) => <Title className={`title`}>{children}</Title>
+export default ({ children }) => <Title className={`title`}>{children}</Title>;
