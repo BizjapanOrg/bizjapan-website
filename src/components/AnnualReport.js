f3a95b83932c5984ef8a6a489bd34dd05f40727e
hirkame <vjtsx.45afv@gmail.com>
2019-11-16 19:31:38 +0900

    Deleted iframe and added image which links to the report.

--- src/components/AnnualReport.js
+++ src/components/AnnualReport.js
@@ -4,6 +4,8 @@ import React from "react";
 import Container from "./styled/Container";
 import Pulse from "../images/pulse.svg";
 
+import report2018 from "../images/report/2018.jpg";
+
 const AboutUs = () => (
   <Container title="Annual Report in 2018">
     <Pulse
@@ -13,11 +15,22 @@ const AboutUs = () => (
         margin: `0 auto 2em`
       }}
     />
-    <p>
+    <p style={{ marginBottom: `2em` }}>
       Bizjapan publishes an annual report to introduce members’ activities and
       event participations, through Project Reports and Partner Introductions.
-      Please have a look to have a grasp of our experiences.
+      Please have a look to have a grasp of our experiences from the image
+      below.
     </p>
+    <a
+      target="_blank"
+      href="https://drive.google.com/file/d/1hQuBPJQtIK6RK50JSSroBUkVjE2cFIZB/view?usp=sharing"
+    >
+      <img
+        src={report2018}
+        alt="Annual Report 2018"
+        style={{ width: `100%` }}
+      />
+    </a>
   </Container>
 );
 
