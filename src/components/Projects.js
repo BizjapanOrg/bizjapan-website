a2983674f593e936a79cdbbe754fcb6c365068a3
hirkame <vjtsx.45afv@gmail.com>
2019-11-16 17:06:33 +0900

    Created Projects Component which render all the images & data of projects in 2018.

---
+++ src/components/Projects.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+// Import styled components
+import Container from "./styled/Container";
+import ProjectCardWithatsbyImage from "./styled/ProjectCardWithatsbyImage";
+import Pulse from "../images/pulse.svg";
+
+var projectsData = require("./projectsData");
+
+const WhatIsProject = () => (
+  <StaticQuery
+    query={query}
+    // Render all the images.
+    render={data => {
+      return (
+        <Container title="Projects in 2019">
+          <Pulse
+            style={{
+              height: `3em`,
+              display: `block`,
+              margin: `0 auto 2em`
+            }}
+          />
+          <div
+            style={{
+              display: `flex`,
+              flexDirection: `row`,
+              flexWrap: `wrap`,
+              justifyContent: `space-evenly`
+            }}
+          >
+            {data.images.edges.map(image => {
+              const project = projectsData.find(v => {
+                return v.id === image.node.name;
+              });
+
+              return (
+                <ProjectCardWithatsbyImage
+                  img={image.node.childImageSharp.fixed}
+                  key={image.node.name}
+                  name={image.node.name}
+                  desc={project.desc}
+                />
+              );
+            })}
+          </div>
+        </Container>
+      );
+    }}
+  />
+);
+
+// Import all the images in partners folder.
+const query = graphql`
+  query {
+    images: allFile(filter: { relativeDirectory: { eq: "projects/2018" } }) {
+      edges {
+        node {
+          relativePath
+          name
+          childImageSharp {
+            fixed(width: 230, height: 160) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default WhatIsProject;
