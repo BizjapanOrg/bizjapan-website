b0e1560a5c62373b51d313a49fc1233a1c703705
hirkame <vjtsx.45afv@gmail.com>
2019-11-16 17:05:55 +0900

    Created all the data of projects in 2018.

---
+++ src/components/projectsData.js
@@ -0,0 +1,77 @@
+module.exports = [
+  {
+    id: "jumpjapan",
+    name: "Jump Japan",
+    desc:
+      "Providing the real information about studying and working in Japan with students abroad."
+  },
+  {
+    id: "mxspace",
+    name: "M x Space",
+    desc:
+      "Holding space medicine study tours during long holidays in spring and summer In order to realize 'The world guaranteed quality of space life (QOSL)'."
+  },
+  {
+    id: "nichy",
+    name: "nichy",
+    desc:
+      "Promoting exchange in terms of human resources between Japan and China by connecting Japanese university students and Chinese companies."
+  },
+  {
+    id: "locco",
+    name: "LoCCo",
+    desc:
+      "Engaging on social issues of Japanese countryside with “regional revitalization×international students” as a key concept."
+  },
+  {
+    id: "pizzjapan",
+    name: "Pizzjapan",
+    desc:
+      "Making pizza more familiar. Making the act of cooking pizza as a platform where everyone can enjoy."
+  },
+  {
+    id: "sparkafrica",
+    name: "Spark Africa",
+    desc:
+      "Providing insight into Sub-Saharan Africa’s startups and tech scenes."
+  },
+  {
+    id: "chado",
+    name: "Chadō ~The Way of Life~",
+    desc:
+      "Disseminating and consequently popularizing chadō with the aim of deepening the global understanding of Japanese culture."
+  },
+  {
+    id: "japantrip",
+    name: "Japan Trip",
+    desc: "Supplying one-minute lecture videos for Japanese learners."
+  },
+  {
+    id: "artjunction",
+    name: "ART JUNCTION",
+    desc: "Creating a place where one considershow to engage with art."
+  },
+  {
+    id: "careerdesignstudio",
+    name: "Career Design Studio",
+    desc:
+      "Supporting international students and other people from abroad to work in Japan."
+  },
+  {
+    id: "bizx",
+    name: "BizX",
+    desc:
+      "Raising human resources that could cause innovation through teaching data science."
+  },
+  {
+    id: "hcollege",
+    name: "Hassyadai College",
+    desc:
+      "Supporting students from all over Japan to make life choices by themselves with the help of university students."
+  },
+  {
+    id: "zeroichisenior",
+    name: "Zero-ichi Senior",
+    desc: "Collaborate with active students and seniors, and make it happen."
+  }
+];
