480926a5454ab2d4f3e57d2c88b56442851653b9
hirkame <vjtsx.45afv@gmail.com>
2019-11-16 17:30:58 +0900

    Created the component of Past Projects.

---
+++ src/components/PastProjects.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+// Import styled components
+import Container from "./styled/Container";
+import ProjectCard from "./styled/ProjectCardWithatsbyImage";
+import Pulse from "../images/pulse.svg";
+
+var pastProjectsData = require("./pastProjectData");
+
+const PastProjects = () => (
+  <StaticQuery
+    query={query}
+    // Render all the images.
+    render={data => {
+      return (
+        <Container title="Past Projects">
+          <Pulse
+            style={{
+              height: `3em`,
+              display: `block`,
+              margin: `0 auto 2em`
+            }}
+          />
+          <div
+            style={{
+              display: `flex`,
+              flexDirection: `row`,
+              flexWrap: `wrap`,
+              justifyContent: `space-evenly`
+            }}
+          >
+            {data.images.edges.map(image => {
+              const pastProject = pastProjectsData.find(v => {
+                return v.id === image.node.name;
+              });
+
+              return (
+                <ProjectCard
+                  img={image.node.childImageSharp.fixed}
+                  key={image.node.name}
+                  name={image.node.name}
+                  desc={pastProject.desc}
+                />
+              );
+            })}
+          </div>
+        </Container>
+      );
+    }}
+  />
+);
+
+// Import all the images in partners folder.
+const query = graphql`
+  query {
+    images: allFile(filter: { relativeDirectory: { eq: "projects/2017" } }) {
+      edges {
+        node {
+          relativePath
+          name
+          childImageSharp {
+            fixed(width: 230, height: 160) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default PastProjects;
