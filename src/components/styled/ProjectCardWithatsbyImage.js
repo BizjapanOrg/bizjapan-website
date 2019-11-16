0cd678997553ce835ea8571486b776f2befba1e4
hirkame <vjtsx.45afv@gmail.com>
2019-11-16 17:08:45 +0900

    Created ProjectCard component which uses Gatsby Image.

---
+++ src/components/styled/ProjectCardWithatsbyImage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import Img from "gatsby-image";
+
+const ProjectCard = props => (
+  <ProjectCardStyle>
+    <div style={{ borderRadius: `20px 20px 0 0` }}>
+      <Img
+        fixed={props.img}
+        imgStyle={{
+          objectFit: "contain",
+          objectPosition: "50% 50%"
+        }}
+      />
+    </div>
+    <div style={{ margin: `1em` }}>
+      <CardName> {props.name} </CardName>
+      <CardDesc> {props.desc} </CardDesc>
+    </div>
+  </ProjectCardStyle>
+);
+
+// Define style of ProjectCard
+const ProjectCardStyle = styled.div`
+  width: 230px;
+  margin-bottom: 1.45em;
+  border-radius: 20px;
+  box-shadow: 6px 6px 30px 0 rgba(0, 0, 0, 0.16);
+`;
+const CardName = styled.p`
+  margin: 0;
+  font-size: 1.1em;
+  font-weight: bold;
+  text-align: center;
+`;
+const CardDesc = styled.p`
+  margin: 0;
+  padding-top: 0.5em;
+  font-size: 0.8em;
+`;
+
+export default ProjectCard;
