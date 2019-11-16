d73adcbfa909557f23aa6a00996352a1a122e89c
hirkame <vjtsx.45afv@gmail.com>
2019-11-16 17:31:33 +0900

    Gathered all the components on the page.

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 // import Layout from "../components/layout"
 import SEO from "../components/seo";
@@ -6,15 +8,34 @@ import SEO from "../components/seo";
 // Import Components
 import WhatIsProject from "../components/WhatIsProject";
 import Projects from "../components/Projects";
+import PastProjects from "../components/PastProjects";
 
-const SecondPage = () => (
-  // <Layout>
-  <>
-    <SEO title="Projects" />
-    <WhatIsProject />
-    <Projects />
-  </>
-  // </Layout>
-);
+const SecondPage = () => {
+  const ProjectTop = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "topimage/project.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    // <Layout>
+    <>
+      <SEO title="Projects" />
+      <div style={{ paddingTop: `60px` }}>
+        <Img fluid={ProjectTop.placeholderImage.childImageSharp.fluid} />
+      </div>
+      <WhatIsProject />
+      <Projects />
+      <PastProjects />
+    </>
+    // </Layout>
+  );
+};
 
 export default SecondPage;
