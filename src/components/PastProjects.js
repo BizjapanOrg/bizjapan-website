a096216472d36c165ba54587d344b84e7dff55ed
hirkame <vjtsx.45afv@gmail.com>
2019-11-23 00:23:35 +0900

    Internationalized project modals.

--- src/components/PastProjects.js
+++ src/components/PastProjects.js
@@ -1,55 +1,64 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
+import { useIntl } from "gatsby-plugin-intl";
 
 // Import styled components
 import Container from "./styled/Container";
 import ProjectCard from "./styled/ProjectCardWithatsbyImage";
 import Pulse from "../images/pulse.svg";
 
-var pastProjectsData = require("./pastProjectData");
+const PastProjects = () => {
+  const intl = useIntl();
+  // Loaded project data depending on the locale.
+  if (intl.locale === "en") {
+    var pastProjectsData = require("./pastProjectData");
+  } else {
+    pastProjectsData = require("./pastProjectDataJp.js");
+  }
 
-const PastProjects = () => (
-  <StaticQuery
-    query={query}
-    // Render all the images.
-    render={data => {
-      return (
-        <Container title="Past Projects">
-          <Pulse
-            style={{
-              height: `3em`,
-              display: `block`,
-              margin: `0 auto 2em`
-            }}
-          />
-          <div
-            style={{
-              display: `flex`,
-              flexDirection: `row`,
-              flexWrap: `wrap`,
-              justifyContent: `space-evenly`
-            }}
-          >
-            {data.images.edges.map(image => {
-              const pastProject = pastProjectsData.find(v => {
-                return v.id === image.node.name;
-              });
+  return (
+    <StaticQuery
+      query={query}
+      // Render all the images.
+      render={data => {
+        return (
+          <Container title="Past Projects">
+            <Pulse
+              style={{
+                height: `3em`,
+                display: `block`,
+                margin: `0 auto 2em`
+              }}
+            />
+            <div
+              style={{
+                display: `flex`,
+                flexDirection: `row`,
+                flexWrap: `wrap`,
+                justifyContent: `space-evenly`
+              }}
+            >
+              {data.images.edges.map(image => {
+                const pastProject = pastProjectsData.find(v => {
+                  return v.id === image.node.name;
+                });
 
-              return (
-                <ProjectCard
-                  img={image.node.childImageSharp.fixed}
-                  key={image.node.name}
-                  name={pastProject.name}
-                  desc={pastProject.desc}
-                />
-              );
-            })}
-          </div>
-        </Container>
-      );
-    }}
-  />
-);
+                return (
+                  <ProjectCard
+                    img={image.node.childImageSharp.fixed}
+                    key={image.node.name}
+                    name={pastProject.name}
+                    desc={pastProject.desc}
+                  />
+                );
+              })}
+            </div>
+          </Container>
+        );
+      }}
+    />
+  );
+};
 
 // Import all the images in partners folder.
 const query = graphql`
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,55 +1,65 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
+import { useIntl } from "gatsby-plugin-intl";
 
 // Import styled components
 import Container from "./styled/Container";
 import ProjectCard from "./styled/ProjectCardWithatsbyImage";
 import Pulse from "../images/pulse.svg";
 
-var projectsData = require("./projectsData");
+const WhatIsProject = () => {
+  const intl = useIntl();
 
-const WhatIsProject = () => (
-  <StaticQuery
-    query={query}
-    // Render all the images.
-    render={data => {
-      return (
-        <Container title="Projects in 2018">
-          <Pulse
-            style={{
-              height: `3em`,
-              display: `block`,
-              margin: `0 auto 2em`
-            }}
-          />
-          <div
-            style={{
-              display: `flex`,
-              flexDirection: `row`,
-              flexWrap: `wrap`,
-              justifyContent: `space-evenly`
-            }}
-          >
-            {data.images.edges.map(image => {
-              const project = projectsData.find(v => {
-                return v.id === image.node.name;
-              });
+  // Loaded project data depending on the locale.
+  if (intl.locale === "en") {
+    var projectsData = require("./projectsData");
+  } else {
+    projectsData = require("./projectsDataJp");
+  }
 
-              return (
-                <ProjectCard
-                  img={image.node.childImageSharp.fixed}
-                  key={image.node.name}
-                  name={project.name}
-                  desc={project.desc}
-                />
-              );
-            })}
-          </div>
-        </Container>
-      );
-    }}
-  />
-);
+  return (
+    <StaticQuery
+      query={query}
+      // Render all the images.
+      render={data => {
+        return (
+          <Container title="Projects in 2018">
+            <Pulse
+              style={{
+                height: `3em`,
+                display: `block`,
+                margin: `0 auto 2em`
+              }}
+            />
+            <div
+              style={{
+                display: `flex`,
+                flexDirection: `row`,
+                flexWrap: `wrap`,
+                justifyContent: `space-evenly`
+              }}
+            >
+              {data.images.edges.map(image => {
+                const project = projectsData.find(v => {
+                  return v.id === image.node.name;
+                });
+
+                return (
+                  <ProjectCard
+                    img={image.node.childImageSharp.fixed}
+                    key={image.node.name}
+                    name={project.name}
+                    desc={project.desc}
+                  />
+                );
+              })}
+            </div>
+          </Container>
+        );
+      }}
+    />
+  );
+};
 
 // Import all the images in partners folder.
 const query = graphql`
