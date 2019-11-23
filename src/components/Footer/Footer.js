b5f7a6754ecf3b48a35c72121cd7b5e350f67748
hirkame <vjtsx.45afv@gmail.com>
2019-11-23 09:46:33 +0900

    Moved Footer into new Footer folder.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 // Import components.
-import FooterNavi from './FooterNavi';
-import FooterSocialMedia from './FooterSocialMedia';
-import BlackLogo from './BlackLogo';
+import FooterNavi from "./FooterNavi";
+import FooterSocialMedia from "./FooterSocialMedia";
+import BlackLogo from "../BlackLogo";
 
-import Container from './styled/Container';
+import Container from "../styled/Container";
 
 const Footer = () => {
   return (
@@ -30,6 +30,6 @@ const Footer = () => {
   );
 };
 
-var naviList = ['home', 'project', 'report', 'profile', 'join', 'contact'];
+var naviList = ["home", "project", "report", "profile", "join", "contact"];
 
 export default Footer;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import "./layout.css"
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import "./layout.css";
 
 const Layout = ({ children }) => {
   return (
@@ -19,11 +19,11 @@ const Layout = ({ children }) => {
       <main>{children}</main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
