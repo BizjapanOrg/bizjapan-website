c31c9edb0cfaa1289e087d67efe0985d23fbc7d5
hirkame <vjtsx.45afv@gmail.com>
2019-11-23 16:54:03 +0900

    Introduced language switching system.

--- src/components/Header/HeaderNavi.js
+++ src/components/Header/HeaderNavi.js
@@ -1,43 +1,65 @@
 import React from "react";
-import { Link } from "gatsby-plugin-intl";
+import { useIntl, Link, changeLocale } from "gatsby-plugin-intl";
 import styled from "styled-components";
 
-const HeaderNavi = () => (
-  <ul
-    style={{
-      listStyle: `none`,
-      textAlign: `center`,
-      margin: `0`,
-      float: `right`,
-      fontWeight: `bold`,
-      textDecoration: `none`
-    }}
-  >
-    <NaviItem key="home">
-      <ItemLink to={"/"}>HOME</ItemLink>
-    </NaviItem>
-    <NaviItem key="project">
-      <ItemLink to="/project">PROJECT</ItemLink>
-    </NaviItem>
-    <NaviItem key="report">
-      <ItemLink to="/report">REPORT</ItemLink>
-    </NaviItem>
-    <NaviItem key="profile">
-      <ItemLink to="/profile">PROFILE</ItemLink>
-    </NaviItem>
-    <NaviItem key="join">
-      <a
-        style={{ color: `black`, textDecoration: `none` }}
-        href="https://sites.google.com/bizjapan.org/recruiting-jp/home"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="See our recruiting portal page."
-      >
-        JOIN
-      </a>
-    </NaviItem>
-  </ul>
-);
+const HeaderNavi = () => {
+  const intl = useIntl();
+
+  // Loaded project data depending on the locale.
+  if (intl.locale === "en") {
+    var nextlanguage = "ja";
+    var language = "日本語";
+  } else {
+    nextlanguage = "en";
+    language = "English";
+  }
+
+  return (
+    <ul
+      style={{
+        listStyle: `none`,
+        textAlign: `center`,
+        margin: `0`,
+        float: `right`,
+        fontWeight: `bold`,
+        textDecoration: `none`
+      }}
+    >
+      <NaviItem key="home">
+        <ItemLink to={"/"}>HOME</ItemLink>
+      </NaviItem>
+      <NaviItem key="project">
+        <ItemLink to="/project">PROJECT</ItemLink>
+      </NaviItem>
+      <NaviItem key="report">
+        <ItemLink to="/report">REPORT</ItemLink>
+      </NaviItem>
+      <NaviItem key="profile">
+        <ItemLink to="/profile">PROFILE</ItemLink>
+      </NaviItem>
+      <NaviItem key="join">
+        <a
+          style={{ color: `black`, textDecoration: `none` }}
+          href="https://sites.google.com/bizjapan.org/recruiting-jp/home"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="See our recruiting portal page."
+        >
+          JOIN
+        </a>
+      </NaviItem>
+      <NaviItem key="language">
+        <span
+          style={{ color: `black`, textDecoration: `none` }}
+          aria-label="Change the locale."
+          onClick={() => changeLocale(nextlanguage)}
+        >
+          {language}
+        </span>
+      </NaviItem>
+    </ul>
+  );
+};
 
 // Define styled components.
 const NaviItem = styled.li`
--- src/components/Header/HeaderNaviMobile.js
+++ src/components/Header/HeaderNaviMobile.js
@@ -1,73 +1,95 @@
 import React from "react";
-import { Link } from "gatsby-plugin-intl";
+import { Link, changeLocale, useIntl } from "gatsby-plugin-intl";
 import styled from "styled-components";
 
 import BlackLogo from "../BlackLogo";
 
-const HeaderNaviMobile = () => (
-  <div
-    style={{
-      zIndex: `1000000`,
-      position: `fixed`,
-      top: `60px`,
-      left: `0`,
-      width: `100vw`,
-      height: `calc(100vh - 60px)`,
-      padding: `70px`,
-      margin: `0`,
-      backgroundColor: `white`,
-      opacity: `95%`
-    }}
-  >
-    <ul
-      style={{
-        display: `flex`,
-        flexDirection: `column`,
-        margin: `0`,
-        fontWeight: `bold`,
-        listStyle: `none`,
-        textDecoration: `none`,
-        textAlign: `right`
-      }}
-    >
-      <NaviItem key="home">
-        <ItemLink to={"/"}>HOME</ItemLink>
-      </NaviItem>
-      <NaviItem key="project">
-        <ItemLink to="/project">PROJECT</ItemLink>
-      </NaviItem>
-      <NaviItem key="report">
-        <ItemLink to="/report">REPORT</ItemLink>
-      </NaviItem>
-      <NaviItem key="profile">
-        <ItemLink to="/profile">PROFILE</ItemLink>
-      </NaviItem>
-      <NaviItem key="join">
-        <a
-          style={{ color: `black`, textDecoration: `none` }}
-          href="https://sites.google.com/bizjapan.org/recruiting-jp/home"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Check our recruiting portal page."
-        >
-          JOIN
-        </a>
-      </NaviItem>
-    </ul>
+const HeaderNaviMobile = () => {
+  const intl = useIntl();
+
+  // Loaded project data depending on the locale.
+  if (intl.locale === "en") {
+    var nextlanguage = "ja";
+    var language = "日本語";
+  } else {
+    nextlanguage = "en";
+    language = "English";
+  }
+
+  return (
     <div
       style={{
-        position: `absolute`,
-        bottom: `70px`,
-        width: `calc(100% - 140px)`
+        zIndex: `1000000`,
+        position: `fixed`,
+        top: `60px`,
+        left: `0`,
+        width: `100vw`,
+        height: `calc(100vh - 60px)`,
+        padding: `50px 70px`,
+        margin: `0`,
+        backgroundColor: `white`,
+        opacity: `95%`
       }}
     >
-      <h2 style={{ textAlign: `center`, marginBottom: `35px` }}>
-        Global x Entrepreneurship
-      </h2>
-      <BlackLogo />
+      <ul
+        style={{
+          display: `flex`,
+          flexDirection: `column`,
+          margin: `0`,
+          fontWeight: `bold`,
+          listStyle: `none`,
+          textDecoration: `none`,
+          textAlign: `right`
+        }}
+      >
+        <NaviItem key="home">
+          <ItemLink to={"/"}>HOME</ItemLink>
+        </NaviItem>
+        <NaviItem key="project">
+          <ItemLink to="/project">PROJECT</ItemLink>
+        </NaviItem>
+        <NaviItem key="report">
+          <ItemLink to="/report">REPORT</ItemLink>
+        </NaviItem>
+        <NaviItem key="profile">
+          <ItemLink to="/profile">PROFILE</ItemLink>
+        </NaviItem>
+        <NaviItem key="join">
+          <a
+            style={{ color: `black`, textDecoration: `none` }}
+            href="https://sites.google.com/bizjapan.org/recruiting-jp/home"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Check our recruiting portal page."
+          >
+            JOIN
+          </a>
+        </NaviItem>
+        <NaviItem key="language">
+          <span
+            style={{ color: `black`, textDecoration: `none` }}
+            aria-label="Change the locale."
+            onClick={() => changeLocale(nextlanguage)}
+          >
+            {language}
+          </span>
+        </NaviItem>
+      </ul>
+      <div
+        style={{
+          position: `absolute`,
+          bottom: `70px`,
+          width: `calc(100% - 140px)`
+        }}
+      >
+        <h2 style={{ textAlign: `center`, marginBottom: `35px` }}>
+          Global x Entrepreneurship
+        </h2>
+        <BlackLogo />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 // Define styled components.
 const NaviItem = styled.li`
